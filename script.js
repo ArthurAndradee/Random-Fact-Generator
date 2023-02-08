bed2478e25f1f450fd2387296173226588e7573e
Arthur <121949480+ArthurAndradee@users.noreply.github.com>
2023-02-08 11:33:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const body = document.getElementsByClassName("body")
-const factHere = document.getElementById("factHere")
-
-let facts = 
-[
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-]
-let selNumber = Math.floor(Math.random() * 5);
-let randomFact = facts[selNumber];
-
-const generateRandomFact = () => {
-    return randomFact;
-};
-
-factHere.textContent=generateRandomFact()
+function random() {    
+    const facts = 
+    [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+    ]
+    const selNumber = Math.floor(Math.random() * 5);
+    const randomFact = facts[selNumber];
+    
+    const generateRandomFact = () => {
+        return randomFact;
+    };
+    
+    console.log(generateRandomFact());
+}
